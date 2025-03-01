aa1d8797fb939977a6bd571b4d1f02f02324169f
Saarthak <saarthaks18@gmail.com>
2025-03-01 15:27:05 -0500

    Font size checking completed

--- saarthak.js
+++ saarthak.js
@@ -1,1 +1,6 @@
-$("div").css("color", "red");
+$("*").each(function() {
+    let fontSize = parseFloat($(this).css("font-size"));
+    if (fontSize < 16) {
+        console.log("Font size of element is too small.", this)
+    }
+});
