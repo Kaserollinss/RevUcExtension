37e93736b55a4dcb488411436f55ce1a2662074c
Saarthak <saarthaks18@gmail.com>
2025-03-01 16:32:57 -0500

    Skipped header tags completed

--- saarthak.js
+++ saarthak.js
@@ -4,3 +4,12 @@ $("*").each(function() {
         console.log("Font size of element is too small.", this)
     }
 });
+
+let lastNumber = 0;
+$("h1, h2, h3, h4, h5, h6").each(function() {
+    let currentNumber = parseInt(this.tagName.substring(1));
+    if (lastNumber > 0 && currentNumber > lastNumber + 1) {
+        console.log("Skipped header tag found.", this);
+    }
+    lastNumber = currentNumber;
+})
