49601fa4573f3b611bbee9327d07a0de170f96d6
Kaserollinss <156269387+Kaserollinss@users.noreply.github.com>
2025-03-01 22:15:25 -0500

    Added sidebar functionality

---
+++ content-script.js
@@ -0,0 +1,8 @@
+const iframe = document.createElement("iframe")
+iframe.width = "400px"
+iframe.height = "850vh"
+iframe.src = "http://127.0.0.1:5500/sidebar.html#" //chrome.runtime
+//iframe.src =  chrome.runtime.getURL('test.html')
+
+iframe.setAttribute('sandbox', 'allow-downloads allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation allow-top-navigation-to-custom-protocols');
+document.body.appendChild(iframe)
